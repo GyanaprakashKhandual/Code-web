3639356d9ca91b4a0bfd543afafe41d9b1fc25cf
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-07 10:06:09 +0530

    Final Port Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const restAssured = require('./test/ra.json');
+
+const app = express();
+
+// Allow CORS only from http://localhost:3000
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // optional, define allowed methods
+  credentials: true // optional, if you're sending cookies or authorization headers
+}));
+
+app.use(express.json());
+
+app.get('/api/qa-engineer', (req, res) => {
+  res.json(restAssured);
+});
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.send('This is the API for the portfolio website.');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
